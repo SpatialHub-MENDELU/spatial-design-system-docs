a0b4e87931bef42f0b61b50c9afd1c136f48a2c1
Ivo Pisarovic <ivo.pisarovic@mendelu.cz>
2026-06-21 11:47:07 +0200

    Improved sample code in playground template.

--- src/public/templates/vue/src/App.vue
+++ src/public/templates/vue/src/App.vue
@@ -1,23 +1,25 @@
 <script setup>
-import "aframe";
-import "spatial-design-system/primitives/ar-menu.js";
-import "spatial-design-system/components/position.js";
-import { stringifyForHTML } from "spatial-design-system/utils/utils.js";
+import 'aframe';
+import 'spatial-design-system/primitives/ar-menu.js';
+import 'spatial-design-system/components/position.js';
+import { stringifyForHTML } from 'spatial-design-system/utils/utils.js';
+import 'spatial-design-system/components/ar/touch-raycaster.js';
 
 const items = [
-  { color: "white", title: "Games", textColor: "black" },
-  { color: "white", title: "Films", textColor: "black" },
-  { color: "white", title: "Books", textColor: "black" },
-  { color: "white", title: "Paintings", textColor: "black" },
+  { color: 'white', title: 'Games', textColor: 'black' },
+  { color: 'white', title: 'Films', textColor: 'black' },
+  { color: 'white', title: 'Books', textColor: 'black' },
+  { color: 'white', title: 'Paintings', textColor: 'black' },
 ];
 </script>
 
 <template>
-  <a-scene>
+  <a-scene touch-raycaster>
     <a-ar-menu
       position="0 1.5 -3"
       visible="true"
-      color="lightblue"
+      color="#018A6C"
+      activecolor="#03FCC6"
       :items="stringifyForHTML(items)"
       layout="circle"
       showtext="true"
